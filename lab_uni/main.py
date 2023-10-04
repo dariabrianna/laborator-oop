fc50ba968caf8c68dcd5f5629664ec1b0e6581d2
dariabrianna <130040559+dariabrianna@users.noreply.github.com>
2023-10-04 13:08:20 +0300

    class faculty

--- lab_uni/main.py
+++ lab_uni/main.py
@@ -1,3 +1,14 @@
 class University:
     def init(self, faculties):
         self.faculties = faculties
+
+faculties_data = {}
+class Faculty:
+    def init(self, name, abbreviation, study_field):
+        self.name = name
+        self. abbreviation = abbreviation
+        swlf.study_field = study_field
+
+    def add_to_university(self):
+        faculties_data[self.name] = []
+
